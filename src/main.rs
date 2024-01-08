757d92ab7e077c5ce5c87fe1f86c7f60467dfb28
Powerlated <zjia08@gmail.com>
2024-01-07 19:44:38 -0500

    MFT reading working, albeit slowly

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 #![feature(int_roundings)]
 
-use ntfs::{Ntfs, KnownNtfsFileRecordNumber};
+use mft::{
+    attribute::{x30::FileNamespace, MftAttributeContent, MftAttributeType},
+    MftParser,
+};
+use ntfs::{attribute_value::NtfsAttributeValue, KnownNtfsFileRecordNumber::*, Ntfs, NtfsReadSeek};
 use std::{
-    *,
     ffi::c_void,
-    io::{Read, Seek, SeekFrom},
+    fs::{File, OpenOptions},
+    io::{Read, Seek, SeekFrom, Write},
     mem::size_of,
     ops::Range,
     str::from_utf8,
+    *,
 };
 use windows::{
     core::{w, PCWSTR},
     Win32::{
-        Foundation::{GENERIC_READ, HANDLE},
+        Foundation::{CloseHandle, GENERIC_READ, HANDLE},
         Storage::FileSystem::*,
         System::{
             Ioctl::{DISK_GEOMETRY, IOCTL_DISK_GET_DRIVE_GEOMETRY},
@@ -61,7 +66,8 @@ impl Read for DiskWrapper {
         // println!("Read length: {}", buf.len());
 
         let bps = self.geometry.BytesPerSector as i64;
-        let sectors = self.virtual_file_ptr / bps..(self.virtual_file_ptr + buf.len() as i64).div_ceil(bps);
+        let sectors =
+            self.virtual_file_ptr / bps..(self.virtual_file_ptr + buf.len() as i64).div_ceil(bps);
         let bytes = sectors.start * bps..sectors.end * bps;
         let read_len = (bytes.end - bytes.start) as usize;
 
@@ -117,6 +123,26 @@ impl Seek for DiskWrapper {
     }
 }
 
+struct ReadSeekNtfsAttributeValue<'a, T>(&'a mut T, NtfsAttributeValue<'a, 'a>);
+
+impl<T> Read for ReadSeekNtfsAttributeValue<'_, T>
+where
+    T: Read + Seek,
+{
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        Ok(self.1.read(&mut self.0, buf)?)
+    }
+}
+
+impl<T> Seek for ReadSeekNtfsAttributeValue<'_, T>
+where
+    T: Read + Seek,
+{
+    fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
+        Ok(self.1.seek(&mut self.0, pos)?)
+    }
+}
+
 fn main() -> Result<(), Box<dyn error::Error>> {
     unsafe {
         println!("Currently mounted logical drives (from GetLogicalDriveStringsW):");
@@ -126,7 +152,8 @@ fn main() -> Result<(), Box<dyn error::Error>> {
             .get(Range {
                 start: 0,
                 end: (len * 2) as usize,
-            }).unwrap();
+            })
+            .unwrap();
         for i in buf.split(|b| *b == 0u16) {
             if i.len() > 0 {
                 println!("{}", String::from_utf16(i)?);
@@ -135,7 +162,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
         let path: PCWSTR = w!(r"\\.\C:");
 
-        let handle = CreateFileW(
+        let disk_handle = CreateFileW(
             path,
             GENERIC_READ.0,
             FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
@@ -145,24 +172,51 @@ fn main() -> Result<(), Box<dyn error::Error>> {
             None,
         )?;
 
-        let mut ds = DiskWrapper::new(handle)?;
+        let mut disk = DiskWrapper::new(disk_handle)?;
 
         // Read 8 byte system ID, should be "NTFS    "
-        let mut buf = vec![0u8; ds.geometry.BytesPerSector as usize];
-        ReadFile(handle, Some(&mut buf), None, None)?;
+        let mut buf = vec![0u8; disk.geometry.BytesPerSector as usize];
+        ReadFile(disk_handle, Some(&mut buf), None, None)?;
 
         println!("System ID: \"{}\"", from_utf8(&buf[3..11])?);
 
-        let ntfs = Ntfs::new(&mut ds)?;
-        let label = ntfs
-            .volume_name(&mut ds)
-            .unwrap()?
-            .name()
-            .to_string();
+        let fs = Ntfs::new(&mut disk)?;
+        let label = fs.volume_name(&mut disk).unwrap()?.name().to_string();
 
         println!("Volume label: {}", label?);
 
-        // ntfs.file(&mut ntfs, KnownNtfsFileRecordNumber::MFT)?.data(fs, data_stream_name)
+        let file = fs.file(&mut disk, MFT as u64)?;
+        let data = file.data(&mut disk, "").unwrap()?;
+        let data_attr = data.to_attribute()?;
+        let mft_data_value = data_attr.value(&mut disk)?;
+
+        println!("MFT size: {}", mft_data_value.len());
+
+        let mut f = File::create("filenames.txt")?;
+
+        let mut read_seek = ReadSeekNtfsAttributeValue(&mut disk, mft_data_value);
+        let mut mft = MftParser::from_read_seek(&mut read_seek, None)?;
+        let mut num_entries = 0;
+        for e in mft.iter_entries() {
+            for a in e?.iter_attributes().filter_map(|attr| attr.ok()) {
+                match a.data {
+                    // MftAttributeContent::AttrX10(standard_info) => {
+                    // println!("\tX10 attribute: {:#?}", standard_info)
+                    // },
+                    MftAttributeContent::AttrX30(a) => {
+                        // if (a.namespace == FileNamespace::Win32) {
+                            // f.write_all(format!("{}\n", a.name).as_bytes())?;
+                        // }
+                        num_entries += 1;
+                    }
+                    _ => {
+                        // println!("\tSome other attribute: {:#?}", attribute)
+                    }
+                }
+            }
+        }
+
+        println!("Win32 filenames in MFT: {}", num_entries);
     }
 
     Ok(())
